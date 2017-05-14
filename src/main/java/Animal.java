5d650bca6e844ad511026444823979d46988f9d8
Viv-Iam <vivian.opondoh@gmail.com>
2017-05-14 18:22:52 +0300

    Modify Animal.java file
    *Add name attribute
    *Add Animal constructer and include name attribute in constructor
    *Add logic to implement getName() method

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -2,4 +2,14 @@ import org.sql2o.*;
 import java.util.List;
 
 public class Animal {
+public String name;
+
+}
+
+public Animal(String name) {
+  this.name = name;
+}
+
+public String getName() {
+  return name;
 }
