f72b42a7afd07bda9a6425ec6ca32dbd0f53084c
Viv-Iam <vivian.opondoh@gmail.com>
2017-05-14 20:51:10 +0300

    Add test in EndangeredAnimalTest.java to ensure getId returns Id of Endangerd Animal

--- src/test/java/EndangeredAnimalTest.java
+++ src/test/java/EndangeredAnimalTest.java
@@ -3,3 +3,16 @@ import java.util.ArrayList;
 import static org.junit.Assert.*;
 import org.junit.*;
 import org.sql2o.*;
+
+public class EndangeredAnimalTest {
+
+  @Rule
+  public DatabaseRule databaseRule = new DatabaseRule();
+
+  @Test
+  public void getId_returnsIdOfEndangeredAnimal_int() {
+    EndangeredAnimal testEndangeredAnimal = new EndangeredAnimal("Lion", EndangeredAnimal.HEALTHY, EndangeredAnimal.ADULT);
+    testEndangeredAnimal.save();
+    assertTrue(testEndangeredAnimal.getId() > 0);
+  }
+}
