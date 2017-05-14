c924a68f9254b4d82cfe63b7c3b5170197644a75
Viv-Iam <vivian.opondoh@gmail.com>
2017-05-14 20:47:40 +0300

    Write attributes in EndangeredAnimal class

--- src/main/java/EndangeredAnimal.java
+++ src/main/java/EndangeredAnimal.java
@@ -1,3 +1,18 @@
 import org.sql2o.*;
 import java.util.List;
 import java.util.ArrayList;
+
+public class EndangeredAnimal extends Animal {
+
+  private String health;
+  private String age;
+
+  public static final String ANIMAL_TYPE = "endangered";
+  public static final String HEALTHY = "distinctly healthy";
+  public static final String AVERAGE = "average";
+  public static final String ILL = "distinctly unhealthy";
+  public static final String ADULT = "adult";
+  public static final String YOUNG = "youthful";
+  public static final String NEWBORN = "newborn";
+
+}
