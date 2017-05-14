f7c598a955d702737a4087885c5eaa105c44ed17
Viv-Iam <vivian.opondoh@gmail.com>
2017-05-14 17:28:34 +0300

    Add a Database Rule ensure Animals and Sightings tables are cleared after each spec

--- src/test/java/DatabaseRule.java
+++ src/test/java/DatabaseRule.java
@@ -1,19 +1,21 @@
 import org.junit.rules.ExternalResource;
 import org.sql2o.*;
 
-// public class DatabaseRule extends ExternalResource {
-//
-//   @Override
-//   protected void before() {
-//     DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/virtual_pets_test", null, null);
-//   }
-//
-//   @Override
-//   protected void after() {
-//     try(Connection con = DB.sql2o.open()) {
-//       String deletePersonsQuery = "DELETE FROM persons *;";
-//       con.createQuery(deletePersonsQuery).executeUpdate();
-//     }
-//   }
-//
-// }
+public class DatabaseRule extends ExternalResource {
+
+  @Override
+  protected void before() {
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wild_tracker_test", "viv-iam", "password");
+  }
+//empties Sightings and Animals tables after each spec
+  @Override
+    protected void after() {
+      try(Connection con = DB.sql2o.open()) {
+        String deleteSightingssQuery = "DELETE FROM persons *;";
+        String deleteAnimalsQuery = "DELETE FROM monsters *;";
+        con.createQuery(deleteSightingssQuery).executeUpdate();
+        con.createQuery(deleteAnimalsQuery).executeUpdate();
+      }
+    }
+
+}
